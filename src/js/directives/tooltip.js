4de0f1154a67bdb0e705cdaec6a4897efba2886b
jerseygooner <nijay@redoxengine.com>
2015-09-13 22:07:13 -0500

    Rewrite rui-tooltips

--- src/js/directives/tooltip.js
+++ src/js/directives/tooltip.js
@@ -1,21 +1,44 @@
 var app = angular.module('ruiComponents');
 
-app.directive('ruiTooltip', [function() {
+app.directive('ruiTooltip', function($document, $compile) {
   return {
     restrict: 'A',
-    scope: {
-      message: '@',
-      data: '=',
-      wrap: '@'
-    },
-    transclude: true,
-    replace: true,
-    templateUrl: 'templates/tooltip.html',
+    scope: true,
+    link: function(scope, element, attrs) {
+      var html = '<div class="rui-tooltip">{{ text }}</div>';
+      var tooltip = $compile(html)(scope);
 
-    link: function(scope, element, attrs, ctrl, linker) {
-      if (scope.data) {
-        scope.message = scope.data;
-      }
+      scope.text = attrs.ruiTooltip;
+
+      $document.find('body').append(tooltip);
+
+      element.bind('mouseover', function(e) {
+        tooltip.addClass('rui-tooltip-show');
+
+        //find the container of our element so that we can position our tooltip
+        var containerPosition = e.target.getBoundingClientRect(),
+          offset = {}, // = tooltip.offset(),
+          tooltipHeight = tooltip.outerHeight(),
+          topOffset = containerPosition.top + window.pageYOffset; //account for scrolling of the window
+          console.log(tooltipHeight);
+        offset.top = topOffset - (containerPosition.height / 2) - tooltipHeight;
+        offset.left = containerPosition.left + (containerPosition.width / 2) - 31;
+
+        tooltip.offset(offset);
+      });
+
+      element.bind('mouseout', function() {
+        tooltip.removeClass('rui-tooltip-show');
+      });
+
+      //keep tooltip active if hovering over its contents
+      tooltip.bind('mouseover', function() {
+        tooltip.addClass('rui-tooltip-show');
+      });
+
+      tooltip.bind('mouseout', function() {
+        tooltip.removeClass('rui-tooltip-show');
+      });
     }
   };
-}]);
+});
